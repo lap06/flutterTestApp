44c60fbcbeac596c453390880f51a69ff8b1fd58
Your Name <you@example.com>
2022-12-28 19:03:16 +0100

    First

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:testflutter/providers/test_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_) => TestProvider())
+
+    ],child:
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -26,6 +32,8 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
+    ,);
+
   }
 }
 
